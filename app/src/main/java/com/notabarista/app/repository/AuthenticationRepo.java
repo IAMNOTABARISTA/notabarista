e03d674994dd7fbb3b4e09fc093b75b8aee7a453
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-10-28 19:18:29 +0530

    service and repository

--- app/src/main/java/com/notabarista/app/repository/AuthenticationRepo.java
+++ app/src/main/java/com/notabarista/app/repository/AuthenticationRepo.java
@@ -1,2 +1,10 @@
-package com.notabarista.app.repository;public interface AuthenticationRepo {
+package com.notabarista.app.repository;
+
+import com.notabarista.app.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthenticationRepo extends JpaRepository<User, Integer> {
+    User findByUserName(String username);
+
+    User findByEmailId(String email);
 }
--- app/src/main/java/com/notabarista/app/service/AuthenticationService.java
+++ app/src/main/java/com/notabarista/app/service/AuthenticationService.java
@@ -1,2 +1,27 @@
-package com.notabarista.app.service;public class AuthenticationService {
+package com.notabarista.app.service;
+
+
+import com.notabarista.app.model.User;
+import com.notabarista.app.repository.AuthenticationRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    @Autowired
+    private AuthenticationRepo repo;
+
+    public User saveUser(User user) {
+        return repo.save(user);
+    }
+
+    public User fetchUserByEmailId(String email) {
+        return repo.findByEmailId(email);
+    }
+
+    public User fetchUserByUserName(String username) {
+        return repo.findByUserName(username);
+    }
 }
+
